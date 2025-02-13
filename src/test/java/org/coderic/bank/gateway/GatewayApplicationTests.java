bc01e09a61fc948f9c876c063a6e5f53bd5cd65a
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-02-12 21:09:12 -0500

    Add Test

--- src/test/java/org/coderic/bank/gateway/GatewayApplicationTests.java
+++ src/test/java/org/coderic/bank/gateway/GatewayApplicationTests.java
@@ -6,7 +6,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GatewayApplicationTests {
 
+	@Autowired
+	private countryClient countryClient;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetCountryCurrency() {
+		String country = "Spain";
+		GetCountryResponse response = countryClient.getCountry(country);
+		assertNotNull(response);
+		assertEquals("EUR", response.getCountry().getCurrency());
 	}
 }
