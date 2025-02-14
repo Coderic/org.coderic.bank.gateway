3e49d3fc96a9ccf8ca1399db422f216ee40df23d
Neftali Yagua <neftaliyagua@fedoraproject.org>
2025-02-13 20:07:19 -0500

    Fix test

--- src/test/java/org/coderic/bank/gateway/GatewayApplicationTests.java
+++ src/test/java/org/coderic/bank/gateway/GatewayApplicationTests.java
@@ -1,13 +1,19 @@
 package org.coderic.bank.gateway;
 
+import org.coderic.bank.gateway.clients.CountryClient;
+import org.coderic.bank.wsdl.GetCountryResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class GatewayApplicationTests {
 
 	@Autowired
-	private countryClient countryClient;
+	private CountryClient countryClient;
 
 	@Test
 	void contextLoads() {
@@ -18,6 +24,6 @@ class GatewayApplicationTests {
 		String country = "Spain";
 		GetCountryResponse response = countryClient.getCountry(country);
 		assertNotNull(response);
-		assertEquals("EUR", response.getCountry().getCurrency());
+		assertEquals("EUR", response.getCountry().getCurrency().toString());
 	}
 }
